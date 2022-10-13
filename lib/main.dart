fe08ca3b10301c421806b30f14afa040672c9278
barikoi <shaunhossain95@gmail.com>
2022-10-13 19:34:51 +0600

    project initialization

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<MqttServerClient> connect() async {
+    MqttServerClient client =
+    MqttServerClient.withPort('retail.bmapsbd.com', '3ytx51ifsHszdWNI', 1883);
+    client.logging(on: true);
+    client.onConnected = onConnected;
+    client.onDisconnected = onDisconnected;
+    client.onSubscribed = onSubscribed;
+    client.onSubscribeFail = onSubscribeFail;
+    client.pongCallback = pong;
+
+    final connMessage = MqttConnectMessage()
+        .authenticateAs('rilus', 'kingbob')
+        .keepAliveFor(60)
+        .withWillTopic('mqtt/request')
+        .withWillMessage('Will message')
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce);
+    client.connectionMessage = connMessage;
+    try {
+      await client.connect();
+    } catch (e) {
+      print('Exception: $e');
+      client.disconnect();
+    }
+
+    client.updates?.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final MqttMessage message = c[0].payload;
+      //final payload = MqttPublishPayload.bytesToString(message.toString());
+
+      //print('Received message:$payload from topic: ${c[0].topic}>');
+    });
+
+    return client;
+  }
+
+
+  // connection succeeded
+  void onConnected() {
+    print('Connected');
+  }
+
+// unconnected
+  void onDisconnected() {
+    print('Disconnected');
+  }
+
+// subscribe to topic succeeded
+  void onSubscribed(String topic) {
+    print('Subscribed topic: $topic');
+  }
+
+// subscribe to topic failed
+  void onSubscribeFail(String topic) {
+    print('Failed to subscribe $topic');
+  }
+
+// unsubscribe succeeded
+  void onUnsubscribed(String topic) {
+    print('Unsubscribed topic: $topic');
+  }
+
+// PING response received
+  void pong() {
+    print('Ping response client callback invoked');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: MaterialButton(onPressed: (){
+          connect();
+        }, child: Text("send"),color: Colors.deepPurple,)
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
