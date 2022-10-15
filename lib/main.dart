e203edba1da944fa175f5ec9e5817c5a1651a4aa
shaun <shaunhossain95@gmail.com>
2022-10-16 01:09:54 +0600

    connect to a private server

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
@@ -8,6 +11,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,6 +26,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -29,70 +34,96 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<MqttServerClient> connect() async {
-    MqttServerClient client =
-    MqttServerClient.withPort('retail.bmapsbd.com', '3ytx51ifsHszdWNI', 1883);
-    client.logging(on: true);
-    client.onConnected = onConnected;
-    client.onDisconnected = onDisconnected;
-    client.onSubscribed = onSubscribed;
-    client.onSubscribeFail = onSubscribeFail;
-    client.pongCallback = pong;
-
-    final connMessage = MqttConnectMessage()
-        .authenticateAs('rilus', 'kingbob')
-        .keepAliveFor(60)
-        .withWillTopic('mqtt/request')
-        .withWillMessage('Will message')
-        .startClean()
-        .withWillQos(MqttQos.atLeastOnce);
-    client.connectionMessage = connMessage;
-    try {
-      await client.connect();
-    } catch (e) {
-      print('Exception: $e');
-      client.disconnect();
-    }
-
-    client.updates?.listen((List<MqttReceivedMessage<MqttMessage>> c) {
-      final MqttMessage message = c[0].payload;
-      //final payload = MqttPublishPayload.bytesToString(message.toString());
-
-      //print('Received message:$payload from topic: ${c[0].topic}>');
-    });
-
-    return client;
+  @override
+  void initState() {
+    connect();
+    super.initState();
   }
 
+  String broker = 'retail.bmapsbd.com';
+  int port = 1883;
+  String username = 'rilus';
+  String passwd = 'kingbob';
+  String clientIdentifier = '3ytx51ifsHszdWNI';
+  double _temp = 20;
+  MqttClient? client;
+  MqttConnectionState? connectionState;
+  StreamSubscription? subscription;
+
+  void connect() async {
+    client = MqttServerClient(broker, clientIdentifier);
+    client?.port = port;
+    client?.logging(on: true);
+    client?.keepAlivePeriod = 30;
+    client?.onConnected = onConnected;
+    client?.onDisconnected = _onDisconnected;
+
+    final MqttConnectMessage connMess = MqttConnectMessage()
+        .withClientIdentifier(clientIdentifier)
+        .startClean() // Non persistent session for testing
+        .keepAliveFor(30)
+        .withWillQos(MqttQos.atMostOnce);
+    client?.connectionMessage = connMess;
+
+    try {
+      await client?.connect(username, passwd);
+    } catch (e) {
+      print(e);
+      _disconnect();
+    }
+
+    if (client?.connectionState == MqttConnectionState.connected) {
+      log('connect to server');
+      setState(() {
+        connectionState = client?.connectionState;
+      });
+    } else {
+      _disconnect();
+    }
+
+    subscription = client?.updates?.listen(_onMessage);
+
+    _subscribeToTopic("temp");
+  }
 
   // connection succeeded
   void onConnected() {
-    print('Connected');
+    print('Connected checked');
   }
 
-// unconnected
-  void onDisconnected() {
-    print('Disconnected');
+  void _subscribeToTopic(String topic) {
+    if (connectionState == MqttConnectionState.connected) {
+      client?.subscribe(topic, MqttQos.exactlyOnce);
+    }
   }
 
-// subscribe to topic succeeded
-  void onSubscribed(String topic) {
-    print('Subscribed topic: $topic');
+  void _onMessage(List<MqttReceivedMessage> event) {
+    final MqttPublishMessage recMess = event[0].payload as MqttPublishMessage;
+    final String message =
+        MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+    log('response -> $recMess');
+
+    setState(() {
+      _temp = double.parse(message);
+    });
   }
 
-// subscribe to topic failed
-  void onSubscribeFail(String topic) {
-    print('Failed to subscribe $topic');
+  void _disconnect() {
+    print('[MQTT client] _disconnect()');
+    client?.disconnect();
+    _onDisconnected();
   }
 
-// unsubscribe succeeded
-  void onUnsubscribed(String topic) {
-    print('Unsubscribed topic: $topic');
-  }
-
-// PING response received
-  void pong() {
-    print('Ping response client callback invoked');
+  void _onDisconnected() {
+    print('[MQTT client] _onDisconnected');
+    setState(() {
+      //topics.clear();
+      connectionState = client?.connectionState;
+      client = null;
+      subscription?.cancel();
+      subscription = null;
+    });
+    print('[MQTT client] MQTT client disconnected');
   }
 
   @override
@@ -102,10 +133,13 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: MaterialButton(onPressed: (){
+          child: MaterialButton(
+        onPressed: () {
           connect();
-        }, child: Text("send"),color: Colors.deepPurple,)
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        },
+        child: Text("send"),
+        color: Colors.deepPurple,
+      )), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
